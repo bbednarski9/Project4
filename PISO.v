5ab227436a6b59e9778c2cb9d42275df8707e3f9
Joanna Mazer <joanna.mazer1@gmail.com>
2016-02-16 13:32:22 -0800

    Serial out variable added

--- PISO.v
+++ PISO.v
@@ -1,16 +1,18 @@
-module PISO(clk, reset, data_in, load_n, data_out); // Parallel in -> Serial Out
+module PISO(clk, reset, data_in, load_n, data_out); // Parallel in -> Serial Out (10 bits)
 
 	input 		clk, reset, load_n;
 	input [9:0] data_in;
 	output 		data_out;
 
 	reg [9:0] 	Q_out;
+	reg 		serial_out;
 
 	always @(posedge clk or posedge reset) begin
 		if (reset) begin
 			Q_out <= 10'b0000000000;
 		end
 		else if (load_n) begin   // shift
+			serial_out <= Q_out[0];
 			Q_out[0] <= Q_out[1];
 			Q_out[1] <= Q_out[2];
 			Q_out[2] <= Q_out[3];
@@ -27,6 +29,6 @@ module PISO(clk, reset, data_in, load_n, data_out); // Parallel in -> Serial Out
 		end
 	end
 
-	assign data_out = Q_out[0]; 
+	assign data_out = serial_out; 
 
 endmodule
